aaf176f649dc61b11f32c87d2549decb67b6680e
Ruslan <ya_man00@mai.ru>
2016-12-05 21:34:23 +0600

    asp

---
+++ WebBook/WebBook/Models/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBook.Models
+{
+    interface IRepository<T> : IDisposable where T : class
+    {
+        void Create(T item);
+        void Update(T item);
+        void Save();
+        void Delete(T item);
+
+        IQueryable<T> Get();      
+        IQueryable<T> Get(Expression<Func<Patient, bool>> predicate);
+    }
+}
---
+++ WebBook/WebBook/Models/PatientContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebBook.Models
+{
+    public class PatientContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+        }
+    }
+}
